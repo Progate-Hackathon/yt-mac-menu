54956c724cfc1aac0e274c3c946c75cd694b81a1
Shunsuke Uchimura <25cm0105@jec.ac.jp>
2026-02-08 15:43:46 +0900

    initial commit

---
+++ yt-mac-menu/yt-mac-menu/yt_mac_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  yt_mac_menuApp.swift
+//  yt-mac-menu
+//
+//  Created by cmStudent on 2026/02/08.
+//
+
+import SwiftUI
+
+@main
+struct yt_mac_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
