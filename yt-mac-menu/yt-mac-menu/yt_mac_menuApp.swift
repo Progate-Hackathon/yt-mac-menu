0efccdb3f310aec62123fb00e05c4334b6fea4ee
Shunsuke Uchimura <25cm0105@jec.ac.jp>
2026-02-12 22:36:29 +0900

    コンフリクトの解消

--- yt-mac-menu/yt-mac-menu/yt_mac_menuApp.swift
+++ yt-mac-menu/yt-mac-menu/yt_mac_menuApp.swift
@@ -9,21 +9,10 @@ import SwiftUI
 
 @main
 struct yt_mac_menuApp: App {
-<<<<<<< HEAD
     @StateObject private var appViewModel = AppViewModel()
     
     var body: some Scene {
         MenuBarExtra("yt-mac-menu", systemImage: "star.fill") {
-=======
-    var body: some Scene {
-        MenuBarExtra("yt-mac-menu", systemImage: "star.fill") {
-            Button("(プレビューなし)検知開始をシミュレート") {
-                CameraWindowController.shared.open()
-            }
-            Button("(プレビューあり)検知開始をシミュレート") {
-                CameraWindowController.shared.open()
-            }
->>>>>>> develop
             SettingsLink()
             Divider()
             Button("Quit") {
