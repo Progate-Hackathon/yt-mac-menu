271e7cb6fa780c879b5dbba65ccfb96a6fb4dbea
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2026-02-12 17:33:47 +0900

    feat: 設定変更を検知する

--- yt-mac-menu/yt-mac-menu/ViewModels/SettingsViewModel.swift
+++ yt-mac-menu/yt-mac-menu/ViewModels/SettingsViewModel.swift
@@ -13,4 +13,21 @@ class SettingsViewModel: ObservableObject {
     @Published var githubToken: String = ""
     
     @Published var settingChanged: Bool = false // 設定変更されたか
+    private var cancellables = Set<AnyCancellable>() // Combineの購読を管理するためのセット
+    
+    init() {
+        self.addListenerToSettingFields()
+    }
+    
+    private func addListenerToSettingFields() {
+        // 設定の変更を検知する
+        Publishers.CombineLatest($selectedProjectPath, $githubToken)
+            .debounce(for: 1, scheduler: DispatchQueue.main) // 変更されて１秒経ってない間でまだ変更されたら、sinkへ行かないように
+            .sink { [weak self] _ in
+                
+                guard let self = self else { return }
+                settingChanged = true
+            }
+            .store(in: &cancellables)
+    }
 }
