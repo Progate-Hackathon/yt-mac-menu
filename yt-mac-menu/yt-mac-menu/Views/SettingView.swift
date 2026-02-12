87dda0478aa97f93a7c842a8fdf815630ae30e48
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2026-02-12 20:49:01 +0900

    UI: 保存ボタンとエラーメッセージのUI実装

--- yt-mac-menu/yt-mac-menu/Views/SettingView.swift
+++ yt-mac-menu/yt-mac-menu/Views/SettingView.swift
@@ -6,12 +6,41 @@ struct SettingsView: View {
         VStack {
             ProjectPathSectionView(selectedProjectPath: $settingsViewModel.selectedProjectPath)
             GitHubTokenSectionView(gitHubAccessToken: $settingsViewModel.githubToken)
+            
+            errorMessage
+            
+            saveButton
+
         }
         .frame(width: 480)
         .padding()
         .onAppear {
             NSApp.activate()
         }
+    }
+    
+    @ViewBuilder
+    private var errorMessage: some View {
+        if let errorMessage = settingsViewModel.settingErrorMessage {
+            Text(errorMessage)
+                .foregroundStyle(.red)
+        }
+    }
+    
+    @ViewBuilder
+    private var saveButton: some View {
+        let buttonIsDisabled = settingsViewModel.isSaving || !settingsViewModel.settingChanged
+        Button {
+            Task {
+                await settingsViewModel.saveSetting()
+                NSApp.keyWindow?.makeFirstResponder(nil) // 入力フィールドからFocusを消す
+            }
+
+        } label: {
+            Text("保存")
+        }
+        .foregroundStyle(buttonIsDisabled ? .gray : .blue)
+        .disabled(buttonIsDisabled)
     }
 }
 
