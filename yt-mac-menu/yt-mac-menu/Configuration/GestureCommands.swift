1d9c9e6048533ca889b57f39f3dd862a63997ba9
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2026-02-14 01:05:57 +0900

    アーキテクチャ改善を完了 - 古いファイルを削除

--- yt-mac-menu/yt-mac-menu/Configuration/GestureCommands.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum GestureCommand: String {
-    case enableSnap = "enable_snap"
-    case disableSnap = "disable_snap"
-    case enableHeart = "enable_heart"
-    case disableHeart = "disable_heart"
-}
