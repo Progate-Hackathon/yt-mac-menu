52341850aaaf2363aabc47ce2a42715964d74c2d
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2026-02-15 12:23:08 +0900

    feat: GitRepositoryProtocol作成

---
+++ yt-mac-menu/yt-mac-menu/Domain/Repositories/GitRepositoryProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  GitRepositoryProtocol.swift
+//  yt-mac-menu
+//
+//  Created by アウン on 2026/02/14.
+//  Moved to Domain layer for proper architecture
+//
+
+import Foundation
+
+protocol GitRepositoryProtocol {
+    func getRepositoryName(projectPath: String) throws -> String
+    func getCurrentBranch(projectPath: String) throws -> String
+    func getDiff(projectPath: String) throws -> String
+    func getOwner(projectPath: String) throws -> String
+    func getChangedFilePaths(projectPath: String) throws -> [String]
+}
