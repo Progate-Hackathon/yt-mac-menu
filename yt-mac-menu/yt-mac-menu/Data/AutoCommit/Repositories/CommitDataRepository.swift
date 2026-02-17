eb6e9ebc38d060cd31d5a0ae4728816a7d19da5c
Shunsuke Uchimura <25cm0105@jec.ac.jp>
2026-02-17 16:51:57 +0900

    datasource側でエラーハンドリングを網羅的に修正したのでrepositoryは dto <--> domainモデルの変換だけをするように修正

--- yt-mac-menu/yt-mac-menu/Data/AutoCommit/Repositories/CommitDataRepository.swift
+++ yt-mac-menu/yt-mac-menu/Data/AutoCommit/Repositories/CommitDataRepository.swift
@@ -4,50 +4,23 @@ import Combine
 class CommitDataRepository: CommitDataRepositoryProtocol {
     private let remoteCommitDataDataSource: RemoteCommitDataDataSourceProtocol
     
-    init(
-        remoteCommitDataDataSource: RemoteCommitDataDataSourceProtocol
-    ) {
+    init(remoteCommitDataDataSource: RemoteCommitDataDataSourceProtocol) {
         self.remoteCommitDataDataSource = remoteCommitDataDataSource
     }
     
     func sendCommitData(_ data: CommitDataModel) async throws -> CommitSuccessModel {
-        let requestDTO = GitOpsRequestDTO(
-            owner: data.owner,
-            repository: data.repository,
-            baseBranch: nil,
-            headBranch: data.headBranch,
-            createPr: nil,
-            files: data.files.mapValues { content in
-                if let content = content {
-                    return .update(content)
-                } else {
-                    return .delete
-                }
-            }
-        )
+        let requestDTO = GitOpsRequestDTO(commitDataModel: data)
+        let successDTO = try await remoteCommitDataDataSource.send(token: data.githubToken, dto: requestDTO)
+        let urlString = "https://github.com/\(successDTO.params.owner)/\(successDTO.params.repository)"
         
-        do {
-            let successDTO = try await remoteCommitDataDataSource.performGitOps(token: data.githubToken, dto: requestDTO)
-            
-            let urlString = "https://github.com/\(successDTO.params.owner)/\(successDTO.params.repository)"
-            
-            guard let repositoryURL = URL(string: urlString) else {
-                throw CommitError.networkError(URLError(.badURL))
-            }
-            
-            return CommitSuccessModel(
-                repositoryURL: repositoryURL,
-                status: successDTO.status
-            )
-            
-        } catch let dtoError as GitOpsErrorDTO {
-            throw CommitError.remoteError(
-                message: dtoError.message,
-                detail: dtoError.details?.context?.values.joined(separator: ", ")
-            )
-            
-        } catch {
-            throw CommitError.networkError(error)
+        guard let repositoryURL = URL(string: urlString) else {
+            throw CommitError.decodingError(URLError(.badURL))
         }
+        
+        return CommitSuccessModel(
+            repositoryURL: repositoryURL,
+            status: successDTO.status
+        )
     }
 }
+
